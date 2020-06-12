625faedd9e05177e0cb9d187de7ae821500f7fdc
CuriousGeek <krit.s255@gmail.com>
2020-06-11 21:08:04 -0400

    added controller, Dao and service for User story 2.

---
+++ Excaliber/src/main/java/com/revature/data/BatchTechnicalStatusBySkillCategoryDAO.java
@@ -0,0 +1,18 @@
+package com.revature.data;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.revature.tables.BatchTechnicalStatusBySkillCategory;
+
+public class BatchTechnicalStatusBySkillCategoryDAO {
+	public Logger log = Logger.getLogger(BatchTechnicalStatusBySkillCategoryDAO.class);
+	
+	public List<BatchTechnicalStatusBySkillCategory> getTableData(String batchId){
+		
+		//TODO table data logic here
+		
+		return null;
+	}
+}

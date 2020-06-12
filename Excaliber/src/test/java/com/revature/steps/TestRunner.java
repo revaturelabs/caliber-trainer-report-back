78174ea06a50d17820a18b6df7f18db2275cb587
jadams95 <adamsjt95@gmail.com>
2020-06-12 13:00:35 -0400

    new changes

--- Excaliber/src/test/java/com/revature/steps/TestRunner.java
+++ Excaliber/src/test/java/com/revature/steps/TestRunner.java
@@ -2,10 +2,10 @@ package com.revature.steps;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-//(
-//		features = {"src/test/resources/CatAppHome.feature", "src/test/resources/PokeAPI.feature"},
-//		glue = {"com.revature.steps"}
-//)
+(
+		features = {"src/test/resources/CatAppHome.feature", "src/test/resources/PokeAPI.feature"},
+		glue = {"com.revature.steps"}
+)
 @RunWith(value = null)
 public class TestRunner extends AbstractTestNGCucumberTests {
 	

5a5eaa6939d222750b93111e64649acee0bbc28c
CuriousGeek <krit.s255@gmail.com>
2020-06-15 17:03:45 -0400

    so could pull from dev

--- Excaliber/src/main/java/com/revature/services/BatchTechnicalStatusBySkillCategoryService.java
+++ Excaliber/src/main/java/com/revature/services/BatchTechnicalStatusBySkillCategoryService.java
@@ -5,19 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.revature.beans.Trainer;
 import com.revature.data.BatchTechnicalStatusBySkillCategoryDAO;
 import com.revature.tables.BatchTechnicalStatusBySkillCategory;
 
 @Service
 public class BatchTechnicalStatusBySkillCategoryService {
-	BatchTechnicalStatusBySkillCategoryDAO dao;
+	private final StoreRetrieveService sRserv;
 	
 	@Autowired
-	public BatchTechnicalStatusBySkillCategoryService(BatchTechnicalStatusBySkillCategoryDAO v) {
-		dao = v;
+	public BatchTechnicalStatusBySkillCategoryService(StoreRetrieveService v) {
+		this.sRserv = v;
 	}
 	
-	public List<BatchTechnicalStatusBySkillCategory> getTableData(){
-		return dao.getTableData();
+	public List<BatchTechnicalStatusBySkillCategory> getTableData(int id){
+			Trainer trainer = sRserv.getTrainerById(id);
+		
+		return null;
 	}
 }

efd245266f0f00158201314cec39b5f2655efc8a
MJM Builds <meadmichaelj@gmail.com>
2020-06-17 15:30:11 -0400

    reworked user-story-3 service for revised graph

--- Excaliber/src/main/java/com/revature/services/TechnicalStatusByWeek_Service.java
+++ Excaliber/src/main/java/com/revature/services/TechnicalStatusByWeek_Service.java
@@ -1,5 +1,6 @@
 package com.revature.services;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,66 +37,90 @@ public class TechnicalStatusByWeek_Service {
      *
      * @return the table data
      */
-    public List<TechnicalStatusByWeek> technicalStatusByWeekTable(int id) {
+    public List<TechnicalStatusByWeek> getTechnicalStatusByWeek(int id) {
 		Trainer trainer = SRSserv.getTrainerById(id);
-		List<TechnicalStatusByWeek> table = new ArrayList<TechnicalStatusByWeek>();
+		List<TechnicalStatusByWeek> dataTransferObject = new ArrayList<TechnicalStatusByWeek>();
 		
 		log.info("TechnicalStatusByWeek_Service: "
 				+ "Getting table data for TechnicalStatusByWeek");
-
+		
 		//for each batch
 		for (Batch b : trainer.getBatches()) {
 			
 			// for each week of that batch
 			for (Week w : b.getWeeks()) {
 				
-				// get percentage of each technical status
-	            int nullSum = 0;
-				int poorSum = 0;
-	            int averageSum = 0;
-	            int goodSum = 0;
-	            int superstarSum = 0;
-				if (w.getTechnicalStatus() != null) {
-					if (w.getTechnicalStatus().contains("null")) {
-                        nullSum++;
-                    } else if (w.getTechnicalStatus().contains("Poor")) {
-                        poorSum++;
-                    } else if (w.getTechnicalStatus().contains("Average")) {
-                        averageSum++;
-                    } else if (w.getTechnicalStatus().contains("Good")) {
-                        goodSum++;
-                    } else if (w.getTechnicalStatus().contains("Superstar")) {
-                        superstarSum++;
-                    }
-                }
-				int totalSum = nullSum + poorSum + averageSum + goodSum + superstarSum; 
-				int nullPercent = (nullSum / totalSum) * 100;
-				int poorPercent = (poorSum / totalSum) * 100;
-				int averagePercent = (averageSum / totalSum) * 100;
-				int goodPercent = (goodSum / totalSum) * 100;
-				int superstarPercent = (superstarSum / totalSum) * 100;
-				
-				// get all categories for that week
-				ArrayList<String> categories = new ArrayList<String>();
-				for (Category c : w.getCategories()) {
-					// get category name
-					categories.add(c.getName());
+				// for each category
+				for (Category category : w.getCategories()) {
+					
+					// check if category is already in table
+					boolean match = false;
+					for (TechnicalStatusByWeek categoryRow : dataTransferObject) {
+						if (category.getName().equals(categoryRow.getCategory())) { // matching category exists
+							match = true;
+							// increment technical status count
+							if (w.getTechnicalStatus() != null) {
+								if (w.getTechnicalStatus().contains("null")) {
+			                        categoryRow.setNullCount(categoryRow.getNullCount() + 1);
+			                    } else if (w.getTechnicalStatus().contains("Poor")) {
+			                    	categoryRow.setPoorCount(categoryRow.getPoorCount() + 1);
+			                    } else if (w.getTechnicalStatus().contains("Average")) {
+			                    	categoryRow.setAverageCount(categoryRow.getAverageCount() + 1);
+			                    } else if (w.getTechnicalStatus().contains("Good")) {
+			                    	categoryRow.setGoodCount(categoryRow.getGoodCount() + 1);
+			                    } else if (w.getTechnicalStatus().contains("Superstar")) {
+			                    	categoryRow.setSuperstarCount(categoryRow.getSuperstarCount() + 1);
+			                    }
+			                }
+							break;
+						}
+					}
+					// check if category match was not found in table
+					if (match == false) {
+						TechnicalStatusByWeek newCategoryRow = new TechnicalStatusByWeek();
+						newCategoryRow.setCategory(category.getName());
+						// increment technical status count
+						if (w.getTechnicalStatus() != null) {
+							if (w.getTechnicalStatus().contains("null")) {
+		                        newCategoryRow.setNullCount(newCategoryRow.getNullCount() + 1);
+		                    } else if (w.getTechnicalStatus().contains("Poor")) {
+		                    	newCategoryRow.setPoorCount(newCategoryRow.getPoorCount() + 1);
+		                    } else if (w.getTechnicalStatus().contains("Average")) {
+		                    	newCategoryRow.setAverageCount(newCategoryRow.getAverageCount() + 1);
+		                    } else if (w.getTechnicalStatus().contains("Good")) {
+		                    	newCategoryRow.setGoodCount(newCategoryRow.getGoodCount() + 1);
+		                    } else if (w.getTechnicalStatus().contains("Superstar")) {
+		                    	newCategoryRow.setSuperstarCount(newCategoryRow.getSuperstarCount() + 1);
+		                    }
+		                }
+						dataTransferObject.add(newCategoryRow);
+					}
 				}
-				
-				// add new TechnicalStatusByWeek(batchName, score, category, weekNumber)
-				TechnicalStatusByWeek row = new TechnicalStatusByWeek();
-				row.setBatchName(b.getBatchName());
-				row.setNullPercent(nullPercent);
-				row.setPoorPercent(poorPercent);
-				row.setAveragePercent(averagePercent);
-				row.setGoodPercent(goodPercent);
-				row.setSuperstarPercent(superstarPercent);
-				row.setCategories(categories.toArray(new String[categories.size()]));
-				row.setWeek(w.getWeekNumber());
-				table.add(row);
 			}
 		}
-		
-		return table;
-    }
+		// calculate and set percentages
+		for (TechnicalStatusByWeek categoryRow : dataTransferObject) {
+			System.out.println("MJM: " + categoryRow.getCategory());
+			// get sum total of data points for current category
+			Double totalDataPoints = 0.0;
+			totalDataPoints += categoryRow.getNullCount();
+			totalDataPoints += categoryRow.getPoorCount();
+			totalDataPoints += categoryRow.getAverageCount();
+			totalDataPoints += categoryRow.getGoodCount();
+			totalDataPoints += categoryRow.getSuperstarCount();
+			// set averages as technical status count / total data points
+			DecimalFormat df = new DecimalFormat("#.##"); // for formatting to two decimal places
+			Double nullPercent = (categoryRow.getNullCount() / totalDataPoints) * 100;
+			categoryRow.setNullAvg(Double.parseDouble(df.format(nullPercent))); // set formatted null percent
+			Double poorPercent = (categoryRow.getPoorCount() / totalDataPoints) * 100;
+			categoryRow.setPoorAvg(Double.parseDouble(df.format(poorPercent))); // set formatted poor percent
+			Double averagePercent = (categoryRow.getAverageCount() / totalDataPoints) * 100;
+			categoryRow.setAverageAvg(Double.parseDouble(df.format(averagePercent))); // set formatted average percent
+			Double goodPercent = (categoryRow.getGoodCount() / totalDataPoints) * 100;
+			categoryRow.setGoodAvg(Double.parseDouble(df.format(goodPercent))); // set formatted good percent
+			Double superstarPercent = (categoryRow.getSuperstarCount() / totalDataPoints) * 100;
+			categoryRow.setSuperstarAvg(Double.parseDouble(df.format(superstarPercent))); // set formatted super star percent
+		}
+		return dataTransferObject;
+    }    
 }
--- Excaliber/src/main/java/com/revature/tables/TechnicalStatusByWeek.java
+++ Excaliber/src/main/java/com/revature/tables/TechnicalStatusByWeek.java
@@ -1,9 +1,6 @@
 package com.revature.tables;
 
-import java.util.Arrays;
-
 /* Technical Status By Week
-
 User Story:
   Percent of each QC technical status according to the 
   categories assigned to a particular week.
@@ -11,119 +8,144 @@ User Story:
 Objective:
   Determine trainer consistencies by using QC scores for a week in particular.
   (a group of categories -> i.e. Week 4 is Servlets/Angular).
- 
-Note: Using a Set of TechnicalStatusByWeek objects, 
-      a graph could group data points by week
-      and display the percentage of each category.
 */
 
 /**
  * The type Technical status by week.
  */
 public class TechnicalStatusByWeek {
-	private String batchName;         // Name of the batch
-	private Integer goodPercent;      // percentage of good technical status
-	private Integer averagePercent;   // percentage of average technical status
-	private Integer poorPercent;      // percentage of poor technical status
-	private Integer superstarPercent; // percentage of super-star technical status
-	private Integer nullPercent;      // percentage of null technical status
-	private String[] categories;      // the categories the percentages represent
-	private String week;              // the week evaluated
+	private String category;        // category of technical status
+	private Integer nullCount;      // number of null technical status
+	private Integer poorCount;      // number of poor technical status
+	private Integer averageCount;   // number of average technical status
+	private Integer goodCount;      // number of good technical status
+	private Integer superstarCount; // number of super-star technical status
+	private Double nullAvg;        // average of null technical status
+	private Double poorAvg;        // average of poor technical status
+	private Double averageAvg;     // average of average technical status
+	private Double goodAvg;        // average of good technical status
+	private Double superstarAvg;   // average of super-star technical status
 	
 	/**
 	 * Instantiates a new Technical status by week.
 	 */
 	public TechnicalStatusByWeek() {
-		batchName = null;
-		goodPercent = null;
-		averagePercent = null;
-		poorPercent = null;
-		superstarPercent = null;
-		nullPercent = null;
-		categories = null;
-		week = null;
+		category = "unknown";
+		nullCount = 0;
+		poorCount = 0;
+		averageCount = 0;
+		goodCount = 0;
+		superstarCount = 0;
+		nullAvg = 0.0;
+		poorAvg = 0.0;
+		averageAvg = 0.0;
+		goodAvg = 0.0;
+		superstarAvg = 0.0;
 	}
 
-	public TechnicalStatusByWeek(String string, double d, String string2, String string3) {
-		// TODO Auto-generated constructor stub
+	public String getCategory() {
+		return category;
 	}
 
-	public String getBatchName() {
-		return batchName;
+	public void setCategory(String category) {
+		this.category = category;
 	}
 
-	public void setBatchName(String batchName) {
-		this.batchName = batchName;
+	public Integer getNullCount() {
+		return nullCount;
 	}
 
-	public Integer getGoodPercent() {
-		return goodPercent;
+	public void setNullCount(Integer nullCount) {
+		this.nullCount = nullCount;
 	}
 
-	public void setGoodPercent(Integer goodPercent) {
-		this.goodPercent = goodPercent;
+	public Integer getPoorCount() {
+		return poorCount;
 	}
 
-	public Integer getAveragePercent() {
-		return averagePercent;
+	public void setPoorCount(Integer poorCount) {
+		this.poorCount = poorCount;
 	}
 
-	public void setAveragePercent(Integer averagePercent) {
-		this.averagePercent = averagePercent;
+	public Integer getAverageCount() {
+		return averageCount;
 	}
 
-	public Integer getPoorPercent() {
-		return poorPercent;
+	public void setAverageCount(Integer averageCount) {
+		this.averageCount = averageCount;
 	}
 
-	public void setPoorPercent(Integer poorPercent) {
-		this.poorPercent = poorPercent;
+	public Integer getGoodCount() {
+		return goodCount;
 	}
 
-	public Integer getSuperstarPercent() {
-		return superstarPercent;
+	public void setGoodCount(Integer goodCount) {
+		this.goodCount = goodCount;
 	}
 
-	public void setSuperstarPercent(Integer superstarPercent) {
-		this.superstarPercent = superstarPercent;
+	public Integer getSuperstarCount() {
+		return superstarCount;
 	}
 
-	public Integer getNullPercent() {
-		return nullPercent;
+	public void setSuperstarCount(Integer superstarCount) {
+		this.superstarCount = superstarCount;
 	}
 
-	public void setNullPercent(Integer nullPercent) {
-		this.nullPercent = nullPercent;
+	public Double getNullAvg() {
+		return nullAvg;
 	}
 
-	public String[] getCategories() {
-		return categories;
+	public void setNullAvg(Double nullAvg) {
+		this.nullAvg = nullAvg;
 	}
 
-	public void setCategories(String[] categories) {
-		this.categories = categories;
+	public Double getPoorAvg() {
+		return poorAvg;
 	}
 
-	public String getWeek() {
-		return week;
+	public void setPoorAvg(Double poorAvg) {
+		this.poorAvg = poorAvg;
 	}
 
-	public void setWeek(String week) {
-		this.week = week;
+	public Double getAverageAvg() {
+		return averageAvg;
+	}
+
+	public void setAverageAvg(Double averageAvg) {
+		this.averageAvg = averageAvg;
+	}
+
+	public Double getGoodAvg() {
+		return goodAvg;
+	}
+
+	public void setGoodAvg(Double goodAvg) {
+		this.goodAvg = goodAvg;
+	}
+
+	public Double getSuperstarAvg() {
+		return superstarAvg;
+	}
+
+	public void setSuperstarAvg(Double superstarAvg) {
+		this.superstarAvg = superstarAvg;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((averagePercent == null) ? 0 : averagePercent.hashCode());
-		result = prime * result + ((batchName == null) ? 0 : batchName.hashCode());
-		result = prime * result + Arrays.hashCode(categories);
-		result = prime * result + ((goodPercent == null) ? 0 : goodPercent.hashCode());
-		result = prime * result + ((nullPercent == null) ? 0 : nullPercent.hashCode());
-		result = prime * result + ((poorPercent == null) ? 0 : poorPercent.hashCode());
-		result = prime * result + ((superstarPercent == null) ? 0 : superstarPercent.hashCode());
-		result = prime * result + ((week == null) ? 0 : week.hashCode());
+		result = prime * result + ((averageAvg == null) ? 0 : averageAvg.hashCode());
+		result = prime * result + ((averageCount == null) ? 0 : averageCount.hashCode());
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((goodAvg == null) ? 0 : goodAvg.hashCode());
+		result = prime * result + ((goodCount == null) ? 0 : goodCount.hashCode());
+		result = prime * result + ((nullAvg == null) ? 0 : nullAvg.hashCode());
+		result = prime * result + ((nullCount == null) ? 0 : nullCount.hashCode());
+		result = prime * result + ((poorAvg == null) ? 0 : poorAvg.hashCode());
+		result = prime * result + ((poorCount == null) ? 0 : poorCount.hashCode());
+		result = prime * result + ((superstarAvg == null) ? 0 : superstarAvg.hashCode());
+		result = prime * result + ((superstarCount == null) ? 0 : superstarCount.hashCode());
 		return result;
 	}
 
@@ -134,52 +156,70 @@ public class TechnicalStatusByWeek {
 		if (!(obj instanceof TechnicalStatusByWeek))
 			return false;
 		TechnicalStatusByWeek other = (TechnicalStatusByWeek) obj;
-		if (averagePercent == null) {
-			if (other.averagePercent != null)
+		if (averageAvg == null) {
+			if (other.averageAvg != null)
 				return false;
-		} else if (!averagePercent.equals(other.averagePercent))
+		} else if (!averageAvg.equals(other.averageAvg))
 			return false;
-		if (batchName == null) {
-			if (other.batchName != null)
+		if (averageCount == null) {
+			if (other.averageCount != null)
 				return false;
-		} else if (!batchName.equals(other.batchName))
+		} else if (!averageCount.equals(other.averageCount))
 			return false;
-		if (!Arrays.equals(categories, other.categories))
-			return false;
-		if (goodPercent == null) {
-			if (other.goodPercent != null)
+		if (category == null) {
+			if (other.category != null)
 				return false;
-		} else if (!goodPercent.equals(other.goodPercent))
+		} else if (!category.equals(other.category))
 			return false;
-		if (nullPercent == null) {
-			if (other.nullPercent != null)
+		if (goodAvg == null) {
+			if (other.goodAvg != null)
 				return false;
-		} else if (!nullPercent.equals(other.nullPercent))
+		} else if (!goodAvg.equals(other.goodAvg))
 			return false;
-		if (poorPercent == null) {
-			if (other.poorPercent != null)
+		if (goodCount == null) {
+			if (other.goodCount != null)
 				return false;
-		} else if (!poorPercent.equals(other.poorPercent))
+		} else if (!goodCount.equals(other.goodCount))
 			return false;
-		if (superstarPercent == null) {
-			if (other.superstarPercent != null)
+		if (nullAvg == null) {
+			if (other.nullAvg != null)
 				return false;
-		} else if (!superstarPercent.equals(other.superstarPercent))
+		} else if (!nullAvg.equals(other.nullAvg))
 			return false;
-		if (week == null) {
-			if (other.week != null)
+		if (nullCount == null) {
+			if (other.nullCount != null)
 				return false;
-		} else if (!week.equals(other.week))
+		} else if (!nullCount.equals(other.nullCount))
+			return false;
+		if (poorAvg == null) {
+			if (other.poorAvg != null)
+				return false;
+		} else if (!poorAvg.equals(other.poorAvg))
+			return false;
+		if (poorCount == null) {
+			if (other.poorCount != null)
+				return false;
+		} else if (!poorCount.equals(other.poorCount))
+			return false;
+		if (superstarAvg == null) {
+			if (other.superstarAvg != null)
+				return false;
+		} else if (!superstarAvg.equals(other.superstarAvg))
+			return false;
+		if (superstarCount == null) {
+			if (other.superstarCount != null)
+				return false;
+		} else if (!superstarCount.equals(other.superstarCount))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "TechnicalStatusByWeek [batchName=" + batchName + ", goodPercent=" + goodPercent + ", averagePercent="
-				+ averagePercent + ", poorPercent=" + poorPercent + ", superstarPercent=" + superstarPercent
-				+ ", nullPercent=" + nullPercent + ", categories=" + Arrays.toString(categories) + ", week=" + week
-				+ "]";
+		return "TechnicalStatusByWeek [category=" + category + ", nullCount=" + nullCount + ", poorCount=" + poorCount
+				+ ", averageCount=" + averageCount + ", goodCount=" + goodCount + ", superstarCount=" + superstarCount
+				+ ", nullAvg=" + nullAvg + ", poorAvg=" + poorAvg + ", averageAvg=" + averageAvg + ", goodAvg="
+				+ goodAvg + ", superstarAvg=" + superstarAvg + "]";
 	}
 
 }

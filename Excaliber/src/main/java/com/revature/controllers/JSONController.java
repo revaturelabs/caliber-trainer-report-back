47e15cf17a95b086c348e5397d47272006a2bcc4
Multezem Kedir <multekedir@users.noreply.github.com>
2020-06-18 14:34:38 -0700

    app running

---
+++ Excaliber/src/main/java/com/revature/controllers/JSONController.java
@@ -0,0 +1,69 @@
+package com.revature.controllers;
+
+import com.revature.beans.Batch;
+import com.revature.beans.Trainer;
+import com.revature.services.StoreRetrieveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.revature.utils.ParseJSON.*;
+
+/**
+ * The type Json controller.
+ */
+@RestController
+@RequestMapping(path = "/JSONController")
+public class JSONController {
+
+    private final StoreRetrieveService SRSserv;
+
+    /**
+     * Instantiates a new Json controller.
+     *
+     * @param s the s
+     */
+    @Autowired
+    public JSONController(StoreRetrieveService s) {
+        SRSserv = s;
+    }
+
+    /**
+     * TODO
+     *
+     * @return string response entity
+     */
+    @GetMapping
+    public ResponseEntity<String> getTrainer2() {
+
+
+        Trainer trainer = getTrainer();
+        List<Batch> batches = getBatch();
+        trainer.setBatches(batches);
+        SRSserv.addEntireTrainer(trainer);
+        return ResponseEntity.ok().build();
+
+    }
+
+    /**
+     * Add trainer boolean.
+     *
+     * @param payload JSON body to be stored in db
+     * @return boolean boolean
+     */
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public Boolean addTrainer(@RequestBody String payload) {
+        //set the JSON to be parsed
+        setJson(payload);
+        //create a trainer from the JSON payload
+        Trainer trainer = getTrainer();
+        //get the batch data
+        List<Batch> batches = getBatch();
+        //give trainer the batches
+        trainer.setBatches(batches);
+        //if trainer created
+        return SRSserv.addEntireTrainer(trainer).getTrainerId() != -1;
+    }
+}

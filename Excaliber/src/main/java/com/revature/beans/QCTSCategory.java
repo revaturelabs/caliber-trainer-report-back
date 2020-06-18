ee5dde19fb536b54a8f3a5e261eef5767932e442
Multezem Kedir <multekedir@users.noreply.github.com>
2020-06-18 08:36:11 -0700

    fixed some code smells

--- Excaliber/src/main/java/com/revature/beans/QCTSCategory.java
+++ Excaliber/src/main/java/com/revature/beans/QCTSCategory.java
@@ -3,66 +3,89 @@ package com.revature.beans;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The type Qcts category.
+ */
 public class QCTSCategory {
-	private String categoryName;
-	private List<BatchTSCategory> batches;
-	
-	public QCTSCategory() {
-		categoryName = "";
-		batches = new ArrayList<BatchTSCategory>();
-	}
+    private String categoryName;
+    private List<BatchTSCategory> batches;
 
-	public String getCategoryName() {
-		return categoryName;
-	}
+    /**
+     * Instantiates a new Qcts category.
+     */
+    public QCTSCategory() {
+        categoryName = "";
+        batches = new ArrayList<BatchTSCategory>();
+    }
 
-	public void setCategoryName(String categoryName) {
-		this.categoryName = categoryName;
-	}
+    /**
+     * Gets category name.
+     *
+     * @return the category name
+     */
+    public String getCategoryName() {
+        return categoryName;
+    }
 
-	public List<BatchTSCategory> getBatches() {
-		return batches;
-	}
+    /**
+     * Sets category name.
+     *
+     * @param categoryName the category name
+     */
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 
-	public void setBatches(List<BatchTSCategory> batches) {
-		this.batches = batches;
-	}
+    /**
+     * Gets batches.
+     *
+     * @return the batches
+     */
+    public List<BatchTSCategory> getBatches() {
+        return batches;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((batches == null) ? 0 : batches.hashCode());
-		result = prime * result + ((categoryName == null) ? 0 : categoryName.hashCode());
-		return result;
-	}
+    /**
+     * Sets batches.
+     *
+     * @param batches the batches
+     */
+    public void setBatches(List<BatchTSCategory> batches) {
+        this.batches = batches;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		QCTSCategory other = (QCTSCategory) obj;
-		if (batches == null) {
-			if (other.batches != null)
-				return false;
-		} else if (!batches.equals(other.batches))
-			return false;
-		if (categoryName == null) {
-			if (other.categoryName != null)
-				return false;
-		} else if (!categoryName.equals(other.categoryName))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((batches == null) ? 0 : batches.hashCode());
+        result = prime * result + ((categoryName == null) ? 0 : categoryName.hashCode());
+        return result;
+    }
 
-	@Override
-	public String toString() {
-		return "QCTSCategory [categoryName=" + categoryName + ", batches=" + batches + "]";
-	}
-	
-	
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        QCTSCategory other = (QCTSCategory) obj;
+        if (batches == null) {
+            if (other.batches != null)
+                return false;
+        } else if (!batches.equals(other.batches))
+            return false;
+        if (categoryName == null) {
+            return other.categoryName == null;
+        } else return categoryName.equals(other.categoryName);
+    }
+
+    @Override
+    public String toString() {
+        return "QCTSCategory [categoryName=" + categoryName + ", batches=" + batches + "]";
+    }
+
+
 }
---
+++ Excaliber/src/main/java/com/revature/services/AssessScoresByCategoryAllBatchesService.java
@@ -0,0 +1,16 @@
+package com.revature.services;
+
+import com.revature.tables.AssessScoresByCategoryAllBatches;
+
+/**
+ * The interface Assess scores by category all batches service.
+ */
+public interface AssessScoresByCategoryAllBatchesService {
+    /**
+     * Gets assess scores by category all batches.
+     *
+     * @param category the category
+     * @return the assess scores by category all batches
+     */
+    AssessScoresByCategoryAllBatches getAssessScoresByCategoryAllBatches(Integer id);
+}

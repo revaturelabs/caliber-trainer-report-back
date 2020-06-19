f4bdecad66790f87ac3a7695402c89b6262823b2
Multezem Kedir <kedirmultezem@gmail.com>
2020-06-19 12:30:50 -0700

    checking error for post json

--- Excaliber/src/main/java/com/revature/controllers/TableController.java
+++ Excaliber/src/main/java/com/revature/controllers/TableController.java
@@ -4,9 +4,11 @@ import com.revature.services.*;
 import com.revature.tables.*;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -64,7 +66,7 @@ public class TableController {
         TechnicalStatusPerBatch table = this.statusPerBatchService.technicalStatusPerBatchTable(trainerId);
         if(table == null)
             return ResponseEntity.notFound().build();
-        return ResponseEntity.ok(table);
+        return ResponseEntity.ok(this.statusPerBatchService.technicalStatusPerBatchTable(trainerId));
     }
 
     /**
--- Excaliber/src/main/java/com/revature/controllers/TrainerController.java
+++ Excaliber/src/main/java/com/revature/controllers/TrainerController.java
@@ -6,6 +6,7 @@ import com.revature.services.StoreRetrieveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,8 +28,11 @@ public class TrainerController {
 
     @GetMapping
     public ResponseEntity<List<Trainer>> getTrainers() {
-
         return ResponseEntity.ok(SRSserv.getTrainers());
+    }
 
+    @GetMapping(path = "/{trainerId}")
+    public ResponseEntity<List<Trainer>> getTrainers(@PathVariable String trainerId) {
+        return ResponseEntity.ok(SRSserv.getTrainers());
     }
 }

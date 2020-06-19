90a290799e01c15ee193caac5d03e8528f1f97ab
Multezem Kedir <multekedir@users.noreply.github.com>
2020-06-19 11:44:46 -0700

    getting trainer

---
+++ Excaliber/src/main/java/com/revature/controllers/TrainerController.java
@@ -0,0 +1,34 @@
+package com.revature.controllers;
+
+
+import com.revature.beans.Trainer;
+import com.revature.services.StoreRetrieveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * The type Json controller.
+ */
+@RestController
+@RequestMapping(path = "/Trainer")
+public class TrainerController {
+
+    private final StoreRetrieveService SRSserv;
+
+    @Autowired
+    public TrainerController(StoreRetrieveService srSserv) {
+        SRSserv = srSserv;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Trainer>> getTrainers() {
+
+        return ResponseEntity.ok(SRSserv.getTrainers());
+
+    }
+}

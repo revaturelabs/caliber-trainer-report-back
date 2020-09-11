d62858f1bd2f3106cb953bf2615e14f8907de911
nate chin <nathannchin@gmail.com>
2020-09-11 11:05:48 -0500

    Mocked backend api call with Starwars api

--- Excaliber/src/main/java/com/revature/controllers/TrainerController.java
+++ Excaliber/src/main/java/com/revature/controllers/TrainerController.java
@@ -1,16 +1,24 @@
 package com.revature.controllers;
 
 
-import com.revature.beans.Trainer;
-import com.revature.services.StoreRetrieveService;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import com.revature.beans.Trainer;
+import com.revature.services.StoreRetrieveService;
 
 /**
  * The type Json controller.
@@ -18,6 +26,8 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/Trainer")
 public class TrainerController {
+	@Autowired
+	RestTemplate restTemplate;
 
     private final StoreRetrieveService SRSserv;
 
@@ -35,4 +45,18 @@ public class TrainerController {
     public ResponseEntity<Trainer> getTrainers(@PathVariable int trainerId) {
         return ResponseEntity.ok(SRSserv.getTrainerById(trainerId));
     }
+    
+    @RequestMapping(path = "/trainer")
+    public String getEndpointTrainers() {
+    	HttpHeaders headers = new HttpHeaders();
+    	headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+    	HttpEntity <String> entity = new HttpEntity<String>(headers);
+    	return restTemplate.exchange("https://swapi.dev/api/people/", HttpMethod.GET, entity, String.class).getBody();
+    	
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+    	return new RestTemplate();
+    }
+    
 }

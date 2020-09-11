732deb5fa9dc37bbef012ce6a7471210536a1367
akempjones <69207838+akempjones@users.noreply.github.com>
2020-09-11 10:22:44 -0600

    Update TrainerController.java

--- Excaliber/src/main/java/com/revature/controllers/TrainerController.java
+++ Excaliber/src/main/java/com/revature/controllers/TrainerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.revature.beans.Batch;
 
 import java.util.List;
 

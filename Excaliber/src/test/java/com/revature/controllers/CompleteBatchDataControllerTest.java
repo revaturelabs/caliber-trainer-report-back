231b5cd738b45cc79402fafca1965ed32d47ea36
Spencer Campbell <campbellspencer614@gmail.com>
2021-01-15 11:29:27 -0500

    fixed completebatch

--- Excaliber/src/test/java/com/revature/controllers/CompleteBatchDataControllerTest.java
+++ Excaliber/src/test/java/com/revature/controllers/CompleteBatchDataControllerTest.java
@@ -3,8 +3,7 @@ package com.revature.controllers;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,20 +11,25 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.client.RestTemplate;
 
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.mock;
 
+import com.revature.Excaliber;
 import com.revature.beans.Batch;
 import com.revature.beans.CompleteBatch;
 
 //@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@ContextConfiguration(classes = Excaliber.class)
 @RunWith(MockitoJUnitRunner.class)
 public class CompleteBatchDataControllerTest {
 	
